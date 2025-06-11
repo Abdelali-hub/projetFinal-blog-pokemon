0f7041adac43838cc528defdfae6da38208cef8c
Abdelali-hub <abdelaliboufrahiderraz@MacBook-Pro-de-abdelali.local>
2025-06-11 16:38:37 +0200

    Etape 3 création du MiddelWare.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
-
+use App\Http\Controllers\ProduitController;
+use Illuminate\Support\Facades\Auth; 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AdminPass;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -12,6 +14,20 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+Route::middleware([AdminPass::class])->group(function () {
+    Route::get('/produits', [ProduitController::class, 'index']);
+    Route::get('/produit/create', [ProduitController::class, 'create']);
+    Route::post('/produit/post', [ProduitController::class, 'store']);
+    Route::get('/produit/edit/{id}', [ProduitController::class, 'edit']);
+    Route::put('/produit/update/{id}', [ProduitController::class, 'update']);
+    Route::delete('/produit/delete/{id}', [ProduitController::class, 'destroy']);
+    // autre route qu'on veut bloquer
+});
+
+Route::get('/produits', [ProduitController::class, 'index']);
+
+Route::get('/produit', [ProduitController::class, 'index'])->middleware([AdminPass::class]);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
